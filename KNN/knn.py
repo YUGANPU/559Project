e6c83b4e2583f912ea2299be6e3c8879198fddf4
YUGANPU <li3370459208@gmail.com>
2022-04-27 03:59:49 +0100

    knn

--- KNN/knn.py
+++ KNN/knn.py
@@ -1,10 +1,19 @@
-from .. import Data_Process
+import Data_Process
 import pandas as pd
+import numpy as np
+import sklearn
+import matplotlib
 
 
-DataTrain = pd.read_csv("./student_performance_train.csv")
-DataTest = pd.read_csv("./student_performance_test.csv")
-Data_Process.binary(DataTrain)
-DataTest.binary(DataTest)
 
-print(DataTest)
+
+
+
+if __name__ == "__main__":
+
+    DataTrain = pd.read_csv("../student_performance_train.csv")
+    DataTest = pd.read_csv("../student_performance_test.csv")
+    Data_Process.binary(DataTrain)
+    Data_Process.binary(DataTest)
+
+    print(DataTest)
